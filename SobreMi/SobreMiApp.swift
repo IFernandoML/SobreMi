be6a9c4ed392789b858352a2d389f9af3bbffd96
Fernando ML <ikellxd@gmail.com>
2024-01-30 13:15:09 -0500

    Initial Commit

---
+++ SobreMi/SobreMiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SobreMiApp.swift
+//  SobreMi
+//
+//  Created by Ikell Mendez on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SobreMiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
